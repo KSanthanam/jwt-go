6b8185a1a270c37afbecb465d67f8e08df2b94ec
KK <KK.Santhanam@gmail.com>
2015-10-17 22:08:17 +1100

    Introduced variable length Parse Parameters & Vulnerability Fix

--- example_test.go
+++ example_test.go
@@ -7,9 +7,12 @@ import (
 )
 
 func ExampleParse(myToken string, myLookupKey func(interface{}) (interface{}, error)) {
-	token, err := jwt.Parse(myToken, func(token *jwt.Token) (interface{}, error) {
-		return myLookupKey(token.Header["kid"])
-	})
+	token, err := jwt.Parse(jwt.ParseParam{
+		TokenString: myToken,
+		Method: jwt.SigningMethodHS256,
+		KeyFunc: func(token *jwt.Token) (interface{}, error) {
+			return myLookupKey(token.Header["kid"])
+		}})
 
 	if err == nil && token.Valid {
 		fmt.Println("Your token is valid.  I like your style.")
@@ -30,9 +33,12 @@ func ExampleNew(mySigningKey []byte) (string, error) {
 }
 
 func ExampleParse_errorChecking(myToken string, myLookupKey func(interface{}) (interface{}, error)) {
-	token, err := jwt.Parse(myToken, func(token *jwt.Token) (interface{}, error) {
-		return myLookupKey(token.Header["kid"])
-	})
+	token, err := jwt.Parse(jwt.ParseParam{
+		TokenString: myToken,
+		Method: jwt.SigningMethodHS256,
+		KeyFunc: func(token *jwt.Token) (interface{}, error) {
+			return myLookupKey(token.Header["kid"])
+		}})
 
 	if token.Valid {
 		fmt.Println("You look nice today")
